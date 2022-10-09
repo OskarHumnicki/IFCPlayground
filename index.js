1df775f4486c21ce01a0581d2ea29ce33d59dfe9
OskarHumnicki <72790748+OskarHumnicki@users.noreply.github.com>
2022-10-09 23:45:34 +0100

    tidy up

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Color } from 'three';
 import { IfcViewerAPI } from 'web-ifc-viewer';
 import { IfcAPI } from 'web-ifc';
-// import { IfcViewerAPI } from 'web-ifc-viewer';
 
 const api = new IfcAPI();
 api.Init();
@@ -11,45 +10,18 @@ viewer.grid.setGrid();
 viewer.axes.setAxes();
 
 async function loadIfc(url) {
-    //await viewer.IFC.setWasmPath("../../../");
     const model = await viewer.IFC.loadIfcUrl(url);
     await viewer.shadowDropper.renderShadow(model.modelID);
     console.log(model)
     return model;
-    
 }
 
-/*
-
-let model1;
-loadIfc('assets/Heat Pump-Horizontal-Elec Heated-Carrier-Packaged-0.5-6 tons.ifc')
-    .then(res => model1 = res)
-    .then(() => console.log("model 1 id:" + model1))
-
-
-
-let model2;
-loadIfc('assets/Fan Coil-ACME-1.5-2.5 ton-FC1525Z.ifc')
-    .then(res => model2 = res)
-    .then(res => console.log("model 2 id:" + model2))
-    //.then(() => api.SetWasmPath("../../../../", false))
-   // .then(() => api.SetGeometryTransformation(model2, [1,0,0,0,0,1,0,0,0,0,1,0,1000000000000000000,10000000000000000,0,1] ))
-   // .then(() => viewer.getElementById(model2));
-
-//console.log("model 1 id: " + model1);
-
-// window.ondblclick = async () => await viewer.IFC.selector.pickIfcItem();
-// window.onmousemove = async () => await viewer.IFC.selector.prePickIfcItem();
-*/
 const urlarray =
     [
     'assets/Heat Pump-Horizontal-Elec Heated-Carrier-Packaged-0.5-6 tons.ifc',
     'assets/Fan Coil-ACME-1.5-2.5 ton-FC1525Z.ifc',
     'assets/Heat Pump-Vertical-Carrier-Packaged-Dual Blower-17.5-30 tons.ifc',
-
     ];
-
-const models = [];
 
 async function loadIfcs(urls) {
 
@@ -66,9 +38,6 @@ async function loadIfcs(urls) {
         })
         ;
     });
-
-    console.log(models);
-
 }
 
 loadIfcs(urlarray);
